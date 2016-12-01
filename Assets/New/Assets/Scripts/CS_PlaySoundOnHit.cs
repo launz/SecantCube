c194ed42de00411d1060f83d2348aca0a366fe3e
Hang <whitewield@126.com>
2016-12-01 14:26:37 -0500

    LIttle Box Particle

--- Assets/New/Assets/Scripts/CS_PlaySoundOnHit.cs
+++ Assets/New/Assets/Scripts/CS_PlaySoundOnHit.cs
@@ -5,22 +5,28 @@ public class CS_PlaySoundOnHit : MonoBehaviour {
 	[SerializeField] AudioClip mySFX;
 	[SerializeField] float myVolume = 0;
 	[SerializeField] float myPitch = 1;
+
+	[Header("Particle")]
 	[SerializeField] GameObject myParticleEffect;
+
+	void Start () {
+		myParticleEffect = Instantiate (myParticleEffect, this.transform) as GameObject; 
+	}
 
 	void OnCollisionEnter (Collision g_collision) {
 		if (g_collision.transform.tag == CS_Global.TAG_PLAYER)
 			return;
 		PlaySFX ();
-		Instantiate(myParticleEffect, transform.position, Quaternion.identity);
-//		Invoke ("DestroyParticle", myParticleEffect, 3f);
+
+		myParticleEffect.GetComponent<ParticleSystem> ().Play ();
 	}
 
 	void OnTriggerEnter (Collider g_other) {
 		if (g_other.tag == CS_Global.TAG_PLAYER)
 			return;
 		PlaySFX ();
-		Instantiate(myParticleEffect, transform.position, Quaternion.identity);
-//		Invoke ("DestroyParticle", myParticleEffect, 3f);
+
+		myParticleEffect.GetComponent<ParticleSystem> ().Play ();
 	}
 
 	public void PlaySFX () {
@@ -29,8 +35,5 @@ public class CS_PlaySoundOnHit : MonoBehaviour {
 		} else
 			CS_AudioManager.Instance.PlaySFX_Pitch (mySFX, myVolume, myPitch);
 	}
-
-//	void DestroyParticle (GameObject particleEffectToDestroy) {
-//		Destroy (particleEffectToDestroy);
-//	}
+		
 }
