4e6c57cfb00e97a68ffd16d94fc2c4afeb2f42e4
Laurenz <laurenzriklin@gmx.at>
2016-12-01 13:58:34 -0500

    started working on impact particle effect
    +level changes

--- Assets/New/Assets/Scripts/CS_PlaySoundOnHit.cs
+++ Assets/New/Assets/Scripts/CS_PlaySoundOnHit.cs
@@ -5,17 +5,22 @@ public class CS_PlaySoundOnHit : MonoBehaviour {
 	[SerializeField] AudioClip mySFX;
 	[SerializeField] float myVolume = 0;
 	[SerializeField] float myPitch = 1;
+	[SerializeField] GameObject myParticleEffect;
 
 	void OnCollisionEnter (Collision g_collision) {
 		if (g_collision.transform.tag == CS_Global.TAG_PLAYER)
 			return;
 		PlaySFX ();
+		Instantiate(myParticleEffect, transform.position, Quaternion.identity);
+//		Invoke ("DestroyParticle", myParticleEffect, 3f);
 	}
 
 	void OnTriggerEnter (Collider g_other) {
 		if (g_other.tag == CS_Global.TAG_PLAYER)
 			return;
 		PlaySFX ();
+		Instantiate(myParticleEffect, transform.position, Quaternion.identity);
+//		Invoke ("DestroyParticle", myParticleEffect, 3f);
 	}
 
 	public void PlaySFX () {
@@ -24,4 +29,8 @@ public class CS_PlaySoundOnHit : MonoBehaviour {
 		} else
 			CS_AudioManager.Instance.PlaySFX_Pitch (mySFX, myVolume, myPitch);
 	}
+
+//	void DestroyParticle (GameObject particleEffectToDestroy) {
+//		Destroy (particleEffectToDestroy);
+//	}
 }
