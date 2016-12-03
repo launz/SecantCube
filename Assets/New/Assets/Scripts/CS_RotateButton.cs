dc4fc10f22f904135ec06f865efaf43dee5cb180
Hang <whitewield@126.com>
2016-12-02 19:56:55 -0500

    rotate level screen shake and button change color

--- Assets/New/Assets/Scripts/CS_RotateButton.cs
+++ Assets/New/Assets/Scripts/CS_RotateButton.cs
@@ -6,9 +6,25 @@ public class CS_RotateButton : MonoBehaviour {
 	[SerializeField] GameObject myRotateCenter;
 	[SerializeField] GameObject myRotateTarget;
 
+	[Header("Color")]
+	[SerializeField] Color myColorNormal;
+	[SerializeField] Color myColorHit;
+	[SerializeField] float myColorSpeed = 1;
+
+	void Start () {
+		this.GetComponent<Renderer> ().material.color = myColorNormal;
+	}
+
+	void Update () {
+		this.GetComponent<Renderer> ().material.color = Color.Lerp (this.GetComponent<Renderer> ().material.color, myColorNormal, Time.deltaTime * myColorSpeed);
+	}
+
 	void OnCollisionEnter(Collision collision) {
 		if (collision.transform.tag == "Player") {
 			myRotateTarget.GetComponent<CS_RotateLevel> ().SetRotation (Quaternion.Euler(this.transform.localRotation.eulerAngles * -1), myRotateCenter.transform);
 		}
+
+		this.GetComponent<Renderer> ().material.color = myColorHit;
+
 	}
 }
--- Assets/New/Assets/Scripts/CS_RotateLevel.cs
+++ Assets/New/Assets/Scripts/CS_RotateLevel.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class CS_RotateLevel : MonoBehaviour {
 
 	[SerializeField] float myRotationSpeed = 1;
+	[SerializeField] float myRotationStopAngle = 5;
 
 	private Quaternion myTargetRotation;
 	private Transform myRotationCenter;
 	private Vector3 myRotationCenterPosition;
+
+	private Camera myCamera;
+
+	[Header("Shake")]
+	[SerializeField] float myShake_Max = 1;
+	[SerializeField] float myShake_Min = 0;
+	[SerializeField] float myShake_Ratio = 120;
+	[SerializeField] float myShake_Speed = 10;
+	private float myShake_Intensity = 0;
 	// Use this for initialization
 	void Start () {
 		myTargetRotation = this.transform.rotation;
+		myCamera = Camera.main;
 	}
 	
 	// Update is called once per frame
@@ -18,11 +29,23 @@ public class CS_RotateLevel : MonoBehaviour {
 		if (myRotationCenter == null)
 			return;
 
+		if (this.transform.rotation == myTargetRotation)
+			return;
+
+		if (Quaternion.Angle(this.transform.rotation, myTargetRotation) < myRotationStopAngle) {
+			Debug.Log (Quaternion.Angle(this.transform.rotation, myTargetRotation));
+			this.transform.rotation = myTargetRotation;
+			this.transform.position = myRotationCenterPosition - myRotationCenter.position + this.transform.position;
+			return;
+		}
+
+		UpdateCameraShake ();
 		Debug.Log("rotate!!!!!!" + myTargetRotation.eulerAngles);
 //		Vector3.Lerp (this.transform.rotation.eulerAngles, myTargetRotation.eulerAngles, Time.deltaTime * myRotationSpeed);
 //		this.transform.rotation = myTargetRotation;
 		this.transform.rotation = Quaternion.Lerp (this.transform.rotation, myTargetRotation, Time.deltaTime * myRotationSpeed);
 		this.transform.position = myRotationCenterPosition - myRotationCenter.position + this.transform.position;
+
 	}
 
 	public void SetRotation (Quaternion g_rotation, Transform g_center) {
@@ -31,5 +54,29 @@ public class CS_RotateLevel : MonoBehaviour {
 		myRotationCenterPosition = g_center.position;
 	}
 
+	private void UpdateCameraShake () {
+		float t_shakeIntensity;
 
+		t_shakeIntensity = myShake_Max -
+			myShake_Ratio / (
+				Quaternion.Angle (this.transform.rotation, myTargetRotation) + (
+					myShake_Ratio / (
+						myShake_Max - myShake_Min
+					)));
+
+		myShake_Intensity = Mathf.Lerp (
+			myShake_Intensity, 
+			t_shakeIntensity, 
+			Time.deltaTime * myShake_Speed
+		);
+
+		myCamera.transform.position = myCamera.transform.position + Random.insideUnitSphere * myShake_Intensity;
+		myCamera.transform.Rotate (
+			Random.Range (-myShake_Intensity, myShake_Intensity), 
+			Random.Range (-myShake_Intensity, myShake_Intensity), 
+			Random.Range (-myShake_Intensity, myShake_Intensity)
+		);
+
+		//		Debug.Log (myShake_Intensity + "myShake_Intensity");
+	}
 }
