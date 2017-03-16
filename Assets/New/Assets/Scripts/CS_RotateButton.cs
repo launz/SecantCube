3c3fdf428e88ecc347fce117f651664a624e3d1c
8ude <crbertelsen@gmail.com>
2017-03-16 14:05:43 -0400

    Making switches glow before pressed

--- Assets/New/Assets/Scripts/CS_RotateButton.cs
+++ Assets/New/Assets/Scripts/CS_RotateButton.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class CS_RotateButton : MonoBehaviour {
 
@@ -8,27 +9,47 @@ public class CS_RotateButton : MonoBehaviour {
 
 	[Header("Color")]
 	[SerializeField] Color myColorNormal;
+	[SerializeField] Color myColorOtherNormal;
 	[SerializeField] Color myColorHit;
+
 	[SerializeField] float myColorSpeed = 1;
 
 	[SerializeField] AudioClip mySFX;
 
+	[SerializeField] bool _wasHit;
+
+	Renderer thisRenderer;
+
+
 	void Start () {
-		this.GetComponent<Renderer> ().material.color = myColorNormal;
+		_wasHit = false;
+		thisRenderer = this.GetComponent<Renderer> ();
+		thisRenderer.material.color = myColorNormal;
 	}
 
 	void Update () {
-		this.GetComponent<Renderer> ().material.color = Color.Lerp (this.GetComponent<Renderer> ().material.color, myColorNormal, Time.deltaTime * myColorSpeed);
+
+		if (!_wasHit) {
+			if (thisRenderer.material.color == myColorNormal) {
+				thisRenderer.material.DOColor (myColorOtherNormal, 2.0f);
+			} else if (thisRenderer.material.color == myColorOtherNormal) {
+				thisRenderer.material.DOColor (myColorNormal, 2.0f);
+			}
+		}
+		//this.GetComponent<Renderer> ().material.color = Color.Lerp (this.GetComponent<Renderer> ().material.color, myColorNormal, Time.deltaTime * myColorSpeed);
 	}
 
 	void OnCollisionEnter(Collision collision) {
 		if (collision.transform.tag == "Player") {
+			_wasHit = true;
+			thisRenderer.material.DOKill ();
+			this.GetComponent<Renderer> ().material.color = myColorHit;
 			myRotateTarget.GetComponent<CS_RotateLevel> ().SetRotation (Quaternion.Euler(this.transform.localRotation.eulerAngles * -1), myRotateCenter.transform);
 			if (mySFX != null)
 				CS_AudioManager.Instance.PlaySFX (mySFX);
 		}
 
-		this.GetComponent<Renderer> ().material.color = myColorHit;
+
 
 	}
 }
